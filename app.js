f9343c5cb039a51267f80170b63bda1702228955
amna-201 <amnamh2000@gmail.com>
2024-04-09 13:11:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    var nav = document.getElementById("navbar-nav");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  }
+    
